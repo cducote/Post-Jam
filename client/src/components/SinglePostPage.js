0a517322c520f1b3acf691689d3e051c94aa703e
Chris Ducote <chrisd@slappy.local>
2018-11-07 11:14:59 -0500

    new comment form created

--- client/src/components/SinglePostPage.js
+++ client/src/components/SinglePostPage.js
@@ -5,7 +5,27 @@ export default class SinglePostPage extends Component {
 
   state = {
     post:{},
-    comments:[]
+    comments:[],
+    newComment: {
+      body: ''
+    }
+  }
+  handleChange = (event) => {
+    const newComment = { ...this.state.newComment }
+    newComment[event.target.name] = event.target.value
+    this.setState({ newComment })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const newComment = { ...this.state.newPost }
+    this.props.addNewComment(newComment)
+    this.setState({
+      newComment: {
+        body: ''
+      }
+    })
+    console.log(newComment)
   }
 
   getPostAndComments = async () => {
@@ -23,6 +43,12 @@ export default class SinglePostPage extends Component {
     this.getPostAndComments()
   }
 
+  addNewComment = async (newComment) => {
+    const cityId = this.props.match.params.cityId
+    const postId = this.props.match.params.postId
+    await axios.post(`/api/cities/${cityId}/posts/${postId}`, newComment)
+    this.getPostAndComments()
+  }
 
   render() {
     const post = this.state.post
@@ -32,17 +58,23 @@ export default class SinglePostPage extends Component {
           <div>
             {comment.body}
           </div>
+          <hr/>
         </div>
       )
     })
     return (
       <div>
+        <h1>Single Post</h1>
         <div>
           {post.title}
         </div>
         <div>
           {post.body}
         </div><hr/>
+        <form onSumbit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" name="body" value={this.state.newComment.body} placeholder='Write a comment...' height='50%' />
+          <button type='submit' value='Add Comment'>Submit</button>
+        </form>
         <div>
           <h3> Comments </h3>
         </div>
