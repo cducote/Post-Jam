a773198d46fc36b8de4ac93cf2875d19a11a1825
Chris Ducote <chrisd@slappy.local>
2018-11-12 16:22:47 -0500

    profile page styled

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -4,8 +4,21 @@ import styled from 'styled-components'
 import EditUserForm from './EditUserForm'
 import { Link } from 'react-router-dom'
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+    .profile {
+      padding-left: 20px;
+    }
+    .link {
+      align-self: center;
+      font-size: 15px;
+      padding-right: 20px;
+    }
+`
 
 const Img = styled.img`
+  padding: 20px;
   border-radius: 50%;
   width: 235px;
   height: 235px;
@@ -14,6 +27,20 @@ const Img = styled.img`
     height: 100px;
     width: 100px;
   }
+`
+const Profile = styled.div`
+  display: flex;
+    .attributes {
+      margin-left: 20px;
+      padding: 30px;
+      font-size: 20px;
+      line-height: 30px;
+    }
+`
+const EditButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
 `
 export default class ProfilePage extends Component {
 
@@ -33,22 +60,30 @@ export default class ProfilePage extends Component {
   }
 
   render() {
-    // const userId = this.state.user.id
     const user = this.state.user
     return (
       <div>
-        <h1>Profile Page {user.name}</h1><hr/>
+        <Header>
+          <div className='profile'><h1>{user.name}'s Page</h1></div>
+          <div className='link'><Link to='/cities'>Cities</Link></div>
+        </Header>
+        <hr/>
+        
+        <Profile>
         <div>
           <Img src={user.pic} alt={user.name}/>
-          <li>age: {user.age}</li>
-          <li>main_instrument: {user.main_instrument}</li>
-          <li>influences: {user.influences}</li>
-          <li>skills: {user.skills}</li>
-          <li>gear: {user.gear}</li>
-          <li>location: {user.location}</li>
-          <li>bio: {user.bio}</li>
         </div>
-        <Link to='/cities'>Cities</Link>
+          <div className='attributes'>
+          <li>Age: {user.age}</li>
+          <li>Main Instrument: {user.main_instrument}</li>
+          <li>Influences: {user.influences}</li>
+          <li>Skills: {user.skills}</li>
+          <li>Gear: {user.gear}</li>
+          <li>Location: {user.location}</li>
+          <li>Bio: {user.bio}</li>
+        </div>
+        </Profile>
+        <EditButtonContainer>
         <EditUserForm 
           editUser={this.editUser}
           user={this.state.user}
@@ -57,6 +92,7 @@ export default class ProfilePage extends Component {
           currentUser={this.setState.currentUser}
           getUser={this.getUser}
           />
+        </EditButtonContainer>
       </div>
     )
   }
